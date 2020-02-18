d8b12ac42fa5f6cbcf70dca2ab3be0f025e64b0d
unknown <barea.aljaziri@gmail.com>
2020-02-18 10:44:45 +0800

    Update Navbar & Contact where users can enter their info

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -3,19 +3,64 @@ import { SocialIcon } from 'react-social-icons';
 
 
 class Contact extends React.Component{
+	 constructor(props) {
+        super(props);
+} 
 	render(){
 	return(
 		<div>
-			<h3>Contact: </h3>
-			<br/>
 			<div class="title">
-			<SocialIcon url="http://linkedin.com/in/barea-aljaziri-021bbb16a" /><i> </i>
-			<SocialIcon url="http://github.com/barea" /><i> </i>
-			<SocialIcon url="http://www.facebook.com/profile.php?id=100010365074457"/><i> </i>
+			<SocialIcon url={this.props.url} /><i> </i>
+			
 			</div>
 		</div>
 	);
 	}
 }
 
-export default Contact;
+class Con extends React.Component{
+	constructor(){
+		super();
+		this.state = { url: "", url2: "", url3:""}
+	}
+
+	render(){
+		return(
+		<div class="title">
+			<h3>Contact: </h3>
+			<div class ="row">
+			<Contact url={this.state.url} />
+			<div>
+			<br/>
+			<input type="text" onChange={(e) => this.setState({url: e.target.value})} /> 
+			</div>
+			</div>
+
+			<br/>
+
+			<div class ="row">
+			<Contact url={this.state.url2} />
+			<div>
+			<br/>
+			<input type="text" onChange={(e) => this.setState({url2: e.target.value})} /> 
+			</div>
+			</div>
+
+			<br/>
+
+			<div class ="row">
+			<Contact url={this.state.url3} />
+			<div>
+			<br/>
+			<input type="text" onChange={(e) => this.setState({url3: e.target.value})} /> 
+			</div>
+			</div>
+			
+
+		</div>
+
+		);
+	}
+}
+
+export default Con;
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,15 +4,18 @@ import {Button} from 'primereact/button';
 import {Link} from 'react-router';
 import prof from '../prof.jpg';
 import {Dialog} from 'primereact/dialog';
+import {Growl} from 'primereact/growl';
+import {FileUpload} from 'primereact/fileupload';;
  
 class Navbar extends React.Component{
 
 	  constructor(props) {
         super(props);
-        this.state = {visible: false, name:"Name....", hist:""};
+        this.state = {visible: false, name:"Name....", hist:"", file: null};
         this.onClick = this.onClick.bind(this);
         this.onHide = this.onHide.bind(this);
         this.onSave = this.onSave.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     onClick() {
@@ -25,7 +28,13 @@ class Navbar extends React.Component{
     onSave(){
     	 this.setState({name: this.state.hist, visible: false});
     }
-    
+   
+    handleChange(event) {
+    this.setState({
+      file: URL.createObjectURL(event.target.files[0])
+    })
+  }
+
 	render(){
 		 const footer = (
             <div>
@@ -34,12 +43,18 @@ class Navbar extends React.Component{
             </div>
         );
 		return(
-			<div >			
-			<img  src={prof}  class="profile" />
+			<div>	
+				
+        		<label class="custom-file-upload">
+        		<img src={this.state.file} class="profile"/>
+        		<input type="file" onChange={this.handleChange}/>
+        		</label>
+			
 			 <Dialog header="Enter your name: " visible={this.state.visible} style={{width: '50vw'}} footer={footer} onHide={this.onHide} maximizable>
-             	<input type="text" onChange={(e) => this.setState({hist: e.target.value})} />
+             	<input type="text" onChange={(e) => this.setState({hist: e.target.value})}  maxLength="15" />
              </Dialog>
-			<div class="title" onClick={this.onClick}><strong>{this.state.name} </strong></div>			      
+			<div class="title" onClick={this.onClick}><strong>{this.state.name} </strong>
+			</div>			      
 				<ul>
 					<li><strong><Link to="about">About</Link></strong></li>
 					<li><strong><Link to="education">Education</Link></strong></li>
